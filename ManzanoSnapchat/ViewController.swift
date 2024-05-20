174eda2da09e39f487cf74bb146e5cb9289fc568
jorgemanzano <jorge.manzano@tecsup.edu.pe>
2024-05-20 16:26:43 -0500

    Initial Commit

---
+++ ManzanoSnapchat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ManzanoSnapchat
+//
+//  Created by Jorge Manuel Manzano Añamuro on 20/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
