95fbd4facd16d83c3cf4a59a2df6ffa940c572ef
jorgemanzano <jorge.manzano@tecsup.edu.pe>
2024-05-20 19:04:39 -0500

    Lab10 tarea

--- ManzanoSnapchat/IniciarSesionViewController.swift
+++ ManzanoSnapchat/IniciarSesionViewController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import FirebaseCore
 import FirebaseAuth
+import GoogleSignIn
 
 class IniciarSesionViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInButton: GIDSignInButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +27,36 @@ class IniciarSesionViewController: UIViewController {
                 print("Se presento el siguiente error \(error)")
             } else {
                 print("Inicio de sesion exitoso")
+            }
+        }
+    }
+    
+    @IBAction func iniciarSesionGoogle(_ sender: Any) {
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { [unowned self] result, error in
+          guard error == nil else {
+            return
+          }
+
+          guard let user = result?.user,
+            let idToken = user.idToken?.tokenString
+          else {
+            return
+          }
+
+          let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: user.accessToken.tokenString)
+            
+            Auth.auth().signIn(with: credential) { (authResult, error) in
+                print("Intentando Iniciar Sesion en Google")
+                if let error = error {
+                    print("Error iniciando sesion en Google: \(error)")
+                    return
+                }
+                print("Inicio de sesion con Google exitoso")
             }
         }
     }
